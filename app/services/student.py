3a34eb816cf5230da3484f80ce06ab54fd2fecaf
RajaGonda <mirrorwebs@gmail.com>
2025-09-07 14:11:26 +0530

    update sub category ison opatins and register issue fixed

--- app/services/student.py
+++ app/services/student.py
@@ -101,7 +101,6 @@ def register_student_by_data(db: Session, student: RegisterStudentRequest, backg
             is_verified=False,
             image="",  # default
             reset_token="",
-            token_expiry="",
         )
         db.add(new_user)
         db.flush()
